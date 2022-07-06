d7c83319e581405a52c47354cc2da1ab348b445f
konf1nement <clearddcreations@gmail.com>
2022-07-06 19:42:59 -0400

    home, consultation, header, footer - complete - mobile rsponsive

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,31 +2,57 @@ import '../App.css';
 import {Link, NavLink} from 'react-router-dom'
 import NavLogo from '../Images/headerlogo.png'
 import BtnDroplet from '../Images/btndropletsvg.svg'
+import React, {useState} from 'react'
 
 const Header = () => {
+
+  const [active, setActive] = useState("nav__menu");
+  const [icon, setIcon] = useState("nav__toggler");
+  const navToggle = () => {
+    if (active === "nav__menu") {
+      setActive("nav__menu nav__active");
+    } else setActive("nav__menu");
+
+    // Icon Toggler
+    if (icon === "nav__toggler") {
+      setIcon("nav__toggler toggle");
+    } else setIcon("nav__toggler");
+  };
+
   return (
-    <div className="header">
-      <div className="nav">
-        <Link className='logo' to='/'>
-          <img src={NavLogo} alt="Home" className="nav-logo" />
-        </Link>
-        <nav className='nav-menu'>
-          <NavLink exact="true" to="/about">
-            <div className="nav-link">About</div>
-          </NavLink>
-          <NavLink exact="true" to="/solutions">
-            <div className="nav-link">Solutions</div>
-          </NavLink>
-          <NavLink exact="true" to="/casestudies">
-            <div className="nav-link">Case Studies</div>
-          </NavLink>
-          <Link to="/consultation" className='consult-btn'>
+    <nav className="nav">
+      <Link className='nav-logo' to='/'>
+        <img src={NavLogo} alt="Home" className="nav-logo-img" />
+      </Link>
+        <ul className={active}>
+          <li className="nav__item">
+            <NavLink exact="true" to="/about">
+              <div className="nav-link">About</div>
+            </NavLink>
+          </li>
+          <li className="nav__item">
+            <NavLink exact="true" to="/solutions">
+              <div className="nav-link">Solutions</div>
+            </NavLink>
+          </li>
+          <li className="nav__item">
+            <NavLink exact="true" to="/casestudies">
+              <div className="nav-link">Case Studies</div>
+            </NavLink>
+          </li>
+          <li className="nav__item">
+            <Link to="/consultation" className='consult-btn'>
                 <img src={BtnDroplet}className="btn-droplet" />
                 <h4>Free Consultation</h4>
-          </Link>
-        </nav>
-      </div>
-    </div>
+            </Link>
+          </li>
+        </ul>
+        <div onClick={navToggle} className={icon}>
+          <div className="line1"></div>
+          <div className="line2"></div>
+          <div className="line3"></div>
+        </div>
+    </nav>
   );
 }
 
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -8,9 +8,9 @@ import ScrollLg from '../Images/scroll-lg-svg.svg'
 import LearnMoreBtn from './LearnMoreBtn'
 import Image1 from '../Images/image1.png'
 import Image2 from '../Images/image2.png'
-import FoodBev from '../Images/foodbev.png'
-import NonProfit from '../Images/nonprofit.png'
-import Cannabis from '../Images/cannabis.png'
+import FoodBev from '../Images/foodbev.jpg'
+import NonProfit from '../Images/nonprofit.jpg'
+import Cannabis from '../Images/cannabis.jpg'
 import Tech from '../Images/technology.jpg'
 import AllTabs from '../Components/Tabs/AllTabs'
 import HeroImg from '../Components/HeroImg'
@@ -21,25 +21,6 @@ import HeroImg from '../Components/HeroImg'
 
 
 const Home = () => {
-    // const infoTab = homepagedata.map(item => {
-    //     return(
-    //         <HomeServiceInfo
-    //             headline={item.headline}
-    //             description={item.description}
-    //             cardImg={item.img}
-    //             cardTitle={item.cardTitle}
-    //             cardBody={item[2].cardBody}
-    //         />,
-    //         <HomeServiceInfo
-    //             headline={item.headline}
-    //             description={item.description}
-    //             cardImg={item.img}
-    //             cardTitle={item.cardTitle}
-    //             cardBody={item[1].cardBody}
-    //         />
-    //     )
-    // })
-
     return(
         <div className="home">
             <div className="container">
@@ -68,19 +49,23 @@ const Home = () => {
             <div className="container">
                 <div className="the-brand-blurb">
                     <div className="the-brand-left">
-                        <h5>The Brand</h5>
-                        <h2>
-                            Clear Creations is a full-service branding
-                            and digital marketing agency
-                        </h2>
+                        <div className="the-brand-left-text">
+                            <h5>The Brand</h5>
+                            <h2>
+                                Clear Creations is a full-service branding
+                                and digital marketing agency
+                            </h2>
+                        </div>
                     </div>
                     <div className="the-brand-right">
-                        <p className="body-lg">
-                            We work with creators, the curious, the driven, the rule breakers,
-                            and pioneers, to create and bring forth new experiences for
-                            consumers across the world.
-                        </p>
-                        <LearnMoreBtn />
+                        <div className="the-brand-right-text">
+                            <p className="body-lg">
+                                We work with creators, the curious, the driven, the rule breakers,
+                                and pioneers, to create and bring forth new experiences for
+                                consumers across the world.
+                            </p>
+                            <LearnMoreBtn />
+                        </div>
                     </div>
                 </div>
             </div>
@@ -88,11 +73,12 @@ const Home = () => {
                 <div className="image1"></div>
                 <div className="image2"></div>
             </div>
-                <div className="container">
+            <div className="container">
                 <div className="random-blurb">
                     <h1>Investing in your brand isn't <span className="emphasize">expensive.</span> <span className="emphasize">Uninformed decisions</span> are.</h1>
                 </div>
-                <div className="industries-section">
+            </div>
+            <div className="industries-section">
                     <div className="industries-section-top">
                         <h5>Our Favorites</h5>
                         <h2>
@@ -103,9 +89,10 @@ const Home = () => {
                         </p>
                     </div>
                     <div className="industry-grid">
-                        <div className="left-grid">
+                        <div className="grid-left">
                             <div className="industry-card food-bev">
-                                <img src={FoodBev}className="industry-img food-bev-img" />
+                                {/* <img src={FoodBev}className="industry-img food-bev-img" /> */}
+                                <div className="food-bev-img"></div>
                                 <div className="card-overlay blur">
                                     <p className="industry-title body-lg">Food & Beverage</p>
                                     <p className="industry-description body-sm">
@@ -117,7 +104,8 @@ const Home = () => {
                                 </div>
                             </div>
                             <div className="industry-card non-profit">
-                                <img src={NonProfit}className="industry-img non-profit-img" />
+                                {/* <img src={NonProfit}className="industry-img non-profit-img" /> */}
+                                <div className="non-profit-img"></div>
                                 <div className="card-overlay blur">
                                     <p className="industry-title body-lg">Non-Profit</p>
                                     <p className="industry-description body-sm">
@@ -128,8 +116,11 @@ const Home = () => {
                                     </p>
                                 </div>
                             </div>
-                            <div className="industry-card food-bev">
-                                <img src={Cannabis}className="industry-img cannabis-img" />
+                        </div>
+                        <div className="grid-right">
+                        <div className="industry-card cannabis">
+                                {/* <img src={Cannabis}className="industry-img cannabis-img" /> */}
+                                <div className="cannabis-img"></div>
                                 <div className="card-overlay blur">
                                     <p className="industry-title body-lg">Cannabis</p>
                                     <p className="industry-description body-sm">
@@ -140,10 +131,9 @@ const Home = () => {
                                     </p>
                                 </div>
                             </div>
-                        </div>
-                        <div className="right-grid">
-                        <div className="industry-card tech">
-                                <img src={Tech}className="industry-img tech-img" />
+                            <div className="industry-card tech">
+                                {/* <img src={Tech}className="industry-img tech-img" /> */}
+                                <div className="tech-img"></div>
                                 <div className="card-overlay blur">
                                     <p className="industry-title body-lg">Technology</p>
                                     <p className="industry-description body-sm">
@@ -157,31 +147,36 @@ const Home = () => {
                         </div>
                     </div>
                 </div>
+            <div className="container">
                 <div className="random-blurb">
                     <h1>We're Fully Invested In Your Brand. <br /> <span className="emphasize">Are You?</span></h1>
-                    <ConsultationBtn />
+                    <ConsultationBtn className='center-btn'/>
                 </div>
                 <div className="what-we-do">
                     <div className="what-we-do-left">
-                        <h5>What We Do</h5>
-                        <h2>
-                            We Create Compelling Brands That Add Value To The Lives Of Consumers.
-                        </h2>
+                        <div className="what-we-do-left-text">
+                            <h5>What We Do</h5>
+                            <h2>
+                                We Create Compelling Brands That Add Value To The Lives Of Consumers.
+                            </h2>
+                        </div>
                     </div>
                     <div className="what-we-do-right">
-                        <p className="body-lg">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident 
-                            aut id magnam, odio accusamus officiis quaerat sint eligendi dolorem 
-                            nemo cumque. Iusto aliquam quisquam a, aliquid nulla eligendi impedit sit.
-                        </p>
-                        <LearnMoreBtn />
+                        <div className="what-we-do-right-text">
+                            <p className="body-lg">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident 
+                                aut id magnam, odio accusamus officiis quaerat sint eligendi dolorem 
+                                nemo cumque. Iusto aliquam quisquam a, aliquid nulla eligendi impedit sit.
+                            </p>
+                            <LearnMoreBtn />
+                        </div>
                     </div>
                 </div>
                 <div className="services-section">
                     <AllTabs/>
                 </div>
                 {/* Insert Blog Section Here */}
-                </div>
+            </div>
         </div>
     )
 }
